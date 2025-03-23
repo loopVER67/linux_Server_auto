f29761720987a27caa9e5079f62ab9bf1cb0ec09
avarell_naw <72969903+loopVER67@users.noreply.github.com>
2025-03-23 14:26:25 +0700

    Update jump.sh

--- jump.sh
+++ jump.sh
@@ -7,10 +7,23 @@ apt install icinga2 monitoring_plugins
 ##############################################
 #           dns                              #
 ##############################################
-ns=(
+
+echo "adding dns server address"
+
+NS=(
 "175.15.#.1"
 "175.15.#.3"
 "8.8.8.8"
 )
 sed -i '/servername d/' /etc/resolv.conf
-for $ns in name
+for name in "${NS[@]}"
+do
+  echo "nameserver $name" >> /etc/resolv.conf
+done
+for name in "${NS[@]}"
+do
+  echo "- $name"
+done
+
+echo "install ido-pgsql"
+apt install icinga2-ido-pgsql
