d3496ad75de3f75b3a329a9459a25e7dfef0f64e
avarell_naw <72969903+loopVER67@users.noreply.github.com>
2025-03-23 14:12:44 +0700

    Update jump.sh

--- jump.sh
+++ jump.sh
@@ -4,4 +4,13 @@ apt update && upgrade -y
 
 apt install openssh-client openssh-server ovpn-server -y
 apt install icinga2 monitoring_plugins
-
+##############################################
+#           dns                              #
+##############################################
+ns=(
+"175.15.#.1"
+"175.15.#.3"
+"8.8.8.8"
+)
+sed -i '/servername d/' /etc/resolv.conf
+for $ns in name
