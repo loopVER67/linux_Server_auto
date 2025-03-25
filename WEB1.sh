084d1cf26986c2e191a90147202d623322537c5c
avarell_naw <72969903+loopVER67@users.noreply.github.com>
2025-03-25 14:07:22 +0700

    Create WEB1.sh

---
+++ WEB1.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt install apache2 openssh-server openssh-client dovecot postfix roundcub -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
