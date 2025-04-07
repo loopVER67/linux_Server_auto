fdcb5fbef78f4b23abdc6e03eebb5c1eb096ec0b
avarell_naw <72969903+loopVER67@users.noreply.github.com>
2025-04-07 12:10:59 +0700

    Update WEB1.sh

--- WEB1.sh
+++ WEB1.sh
@@ -3,19 +3,19 @@ sudo apt install dovecot postfix samba -y
 cd /etc/samba
 
 share_content=$(cat <<EOF
-  [share]
-   comment = Public Folder
-   path = /home/lnx_server/sharing
-   valid users = samba1, lnx_server
-   writable = yes
-   read only = no
-   #guest ok = yes
-   #guest only = no
-   browseable = yes
-   create mask = 0755
-   directory mask = 0755
-   #force create mode = 775
-   #force directory mode = 775
-
+[share]
+comment = Public Folder
+path = /home/lnx_server/sharing
+valid users = samba1, lnx_server
+writable = yes
+read only = no
+#guest ok = yes
+#guest only = no
+browseable = yes
+create mask = 0755
+directory mask = 0755
+#force create mode = 775
+#force directory mode = 775
+EOF
 )
-echo '$share_content' >> smb.conf
+echo "$share_content" >> smb.conf
